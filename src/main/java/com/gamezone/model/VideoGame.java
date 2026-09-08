45d251f8bdfb21e20b09f8d0f794672c1043d81a
GustavoRodriguez <gustavoandresrodriguez@unicesar.edu.co>
2026-09-07 22:27:21 -0500

    feat: implement VideoGame subclass with description

---
+++ src/main/java/com/gamezone/model/VideoGame.java
@@ -0,0 +1,31 @@
+package com.gamezone.model;
+
+public class VideoGame extends Product {
+private String platform;
+private String genre;
+private String ageRating;
+
+public VideoGame(String id, String title, double price, int stock,
+String platform, String genre, String ageRating) {
+super(id, title, price, stock);
+this.platform = platform;
+this.genre = genre;
+this.ageRating = ageRating;
+}
+
+public String getPlatform() { return platform; }
+public void setPlatform(String platform) { this.platform = platform; }
+
+public String getGenre() { return genre; }
+public void setGenre(String genre) { this.genre = genre; }
+
+public String getAgeRating() { return ageRating; }
+public void setAgeRating(String ageRating) { this.ageRating = ageRating; }
+
+@Override
+public String getDescription() {
+return getTitle() + " [Video game] - Platform: " + platform
++ ", Genre: " + genre + ", Age rating: " + ageRating
++ ", Price: " + getPrice() + ", Stock: " + getStock();
+}
+}

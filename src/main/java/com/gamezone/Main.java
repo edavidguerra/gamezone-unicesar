757f71c5bb30de699f9187960f08f8ec6a589b3a
edavidguerra <edavidguerra@unicesar.edu.co>
2026-09-24 22:50:12 -0500

    feat: wire PromotionService into application startup

--- src/main/java/com/gamezone/Main.java
+++ src/main/java/com/gamezone/Main.java
@@ -3,10 +3,12 @@ package com.gamezone;
 import com.gamezone.persistence.AccessoryRepository;
 import com.gamezone.persistence.PersonRepository;
 import com.gamezone.persistence.ProductRepository;
+import com.gamezone.persistence.PromotionRepository;
 import com.gamezone.persistence.SaleRepository;
 import com.gamezone.service.AccessoryService;
 import com.gamezone.service.PersonService;
 import com.gamezone.service.ProductService;
+import com.gamezone.service.PromotionService;
 import com.gamezone.service.SaleService;
 import com.gamezone.ui.ConsoleMenu;
 
@@ -21,12 +23,14 @@ public class Main {
         PersonRepository personRepository = new PersonRepository();
         SaleRepository saleRepository = new SaleRepository();
         AccessoryRepository accessoryRepository = new AccessoryRepository();
+        PromotionRepository promotionRepository = new PromotionRepository();
 
         ProductService productService = new ProductService(productRepository);
         PersonService personService = new PersonService(personRepository);
         AccessoryService accessoryService = new AccessoryService(accessoryRepository);
+        PromotionService promotionService = new PromotionService(promotionRepository);
         SaleService saleService = new SaleService(saleRepository, productService,
-                personService, accessoryService);
+                personService, accessoryService, promotionService);
 
         ConsoleMenu menu = new ConsoleMenu(productService, personService, saleService, accessoryService);
         menu.start();
